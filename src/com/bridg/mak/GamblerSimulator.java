9a11cfdb49d662c87fdbcd7d027df08d4fe15fdd
Makrand <makrandshingare@gmail.com>
2021-11-30 10:18:53 +0530

    Added : Stacke per day and day per bet

--- src/com/bridg/mak/GamblerSimulator.java
+++ src/com/bridg/mak/GamblerSimulator.java
@@ -3,6 +3,10 @@ package com.bridg.mak;
 public class GamblerSimulator {
 
     public static void main(String[] args) {
-        
+        Stakes stakes = new Stakes();
+        stakes.setStakePerDay(100);
+        stakes.setBetPerGame(1);
+        System.out.println("Total Stakes in Day :" + stakes.getStakePerDay());
+        System.out.println("Total bet per mage : " + stakes.getBetPerGame());
     }
 }
